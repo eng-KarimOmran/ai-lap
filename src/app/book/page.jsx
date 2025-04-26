c62564a661dc18ebd9b93ad3386897d3e81d89fe
KareemOmran <eng.karimomran@gmail.com>
2025-04-26 06:42:56 +0300

    f commit

--- src/app/book/page.jsx
+++ src/app/book/page.jsx
@@ -1,11 +1,16 @@
 "use client"
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Heading from '../components/Heading/Heading'
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 export default function Book() {
+  const pages = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13"];
+  const [isMounted, setIsMounted] = useState(false);
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
 
     const settings = {
         dots: true,
@@ -14,75 +19,26 @@ export default function Book() {
         slidesToShow: 1,
         slidesToScroll: 1,
         rtl: true,
-        appendDots: dots => (
-          <div
-            style={{
-              backgroundColor: "transparent",
-              borderRadius: "10px",
-              padding: "10px",
-            }}
-          >
-            <ul style={{ margin: "0px" }}> {dots} </ul>
-          </div>
-        ),
-        customPaging: i => (
-          <div
-            style={{
-              width: "30px",
-              color: "#F18DF2",
-              border: "1px #F18DF2 solid"
-            }}
-          >
-            {(13 - i).toString().replace(/\d/g, d => "٠١٢٣٤٥٦٧٨٩"[d])}
-          </div>
-        )
+        appendDots: dots => (<div className='bg-transparent rounded-xl p-2.5'> <ul> {dots} </ul> </div>),
+        customPaging: i => (<div className='w-8 text-[#F18DF2] border-[1px] border-[#F18DF2]'>{(13 - i).toString().replace(/\d/g, d => "٠١٢٣٤٥٦٧٨٩"[d])}</div>)
       };
 
   return (
     <section className='py-7'>
         <Heading heading={"Book"} />
         <div className='lg:max-w-[50%] max-w-[90%] mx-auto px-2'>
+          {
+          isMounted ? (
           <Slider {...settings} className='text-center pb-10'>
-            <div className='bg-gray-300 rounded-xl overflow-hidden'>
-              <img className='w-full rounded-xl object-center object-cover block' src="/book/Plug_compressed_pages-to-jpg-0001.jpg" />
-            </div>
-            <div className='bg-gray-300 rounded-xl overflow-hidden'>
-              <img className='w-full rounded-xl object-center object-cover block' src="/book/Plug_compressed_pages-to-jpg-0002.jpg" />
-            </div>
-            <div className='bg-gray-300 rounded-xl overflow-hidden'>
-              <img className='w-full rounded-xl object-center object-cover block' src="/book/Plug_compressed_pages-to-jpg-0003.jpg" />
-            </div>
-            <div className='bg-gray-300 rounded-xl overflow-hidden'>
-              <img className='w-full rounded-xl object-center object-cover block' src="/book/Plug_compressed_pages-to-jpg-0004.jpg" />
-            </div>
-            <div className='bg-gray-300 rounded-xl overflow-hidden'>
-              <img className='w-full rounded-xl object-center object-cover block' src="/book/Plug_compressed_pages-to-jpg-0005.jpg" />
-            </div>
-            <div className='bg-gray-300 rounded-xl overflow-hidden'>
-              <img className='w-full rounded-xl object-center object-cover block' src="/book/Plug_compressed_pages-to-jpg-0006.jpg" />
-            </div>
-            <div className='bg-gray-300 rounded-xl overflow-hidden'>
-              <img className='w-full rounded-xl object-center object-cover block' src="/book/Plug_compressed_pages-to-jpg-0007.jpg" />
-            </div>
-            <div className='bg-gray-300 rounded-xl overflow-hidden'>
-              <img className='w-full rounded-xl object-center object-cover block' src="/book/Plug_compressed_pages-to-jpg-0008.jpg" />
-            </div>
-            <div className='bg-gray-300 rounded-xl overflow-hidden'>
-              <img className='w-full rounded-xl object-center object-cover block' src="/book/Plug_compressed_pages-to-jpg-0009.jpg" />
-            </div>
-            <div className='bg-gray-300 rounded-xl overflow-hidden'>
-              <img className='w-full rounded-xl object-center object-cover block' src="/book/Plug_compressed_pages-to-jpg-0010.jpg" />
-            </div>
-            <div className='bg-gray-300 rounded-xl overflow-hidden'>
-              <img className='w-full rounded-xl object-center object-cover block' src="/book/Plug_compressed_pages-to-jpg-0011.jpg" />
-            </div>
-            <div className='bg-gray-300 rounded-xl overflow-hidden'>
-              <img className='w-full rounded-xl object-center object-cover block' src="/book/Plug_compressed_pages-to-jpg-0012.jpg" />
-            </div>
-            <div className='bg-gray-300 rounded-xl overflow-hidden'>
-              <img className='w-full rounded-xl object-center object-cover block' src="/book/Plug_compressed_pages-to-jpg-0013.jpg" />
-            </div>
+            {
+              pages.map((page)=>(
+                <div key={page} className='bg-gray-300 rounded-xl overflow-hidden'>
+                  <img className='w-full rounded-xl object-center object-cover block' src={`/book/Plug_compressed_pages-to-jpg-00${page}.jpg`} />
+                </div>
+              ))
+            }
           </Slider>
+          ) : null}
         </div>
     </section>
   )
--- src/app/components/Footer/Footer.jsx
+++ src/app/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer className='border-t-[1px] border-t-white/80 py-8 bg-black'>
+    <footer className='border-t-[1px] border-t-white/80 py-8 bg-black mt-5'>
       <div className='w-full mx-auto lg:w-[85%]'>
         <div className='text-gray-300 text-center flex justify-center items-center flex-col lg:flex-row gap-3  lg:gap-1'>
           <span>
