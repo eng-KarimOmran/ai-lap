82a82535c692114161290da3f3de69ff0eec2371
KareemOmran <eng.karimomran@gmail.com>
2025-04-28 20:55:14 +0300

    finly2 commit

--- src/app/components/Navbar/Navbar.jsx
+++ src/app/components/Navbar/Navbar.jsx
@@ -23,7 +23,7 @@ export default function Navbar() {
     <nav className={`sticky top-0 start-0 end-0 py-4 z-10 border-b-[1px] border-white/95 bg-black`}>
         <div className='w-full mx-auto lg:w-[85%] px-4 flex justify-between items-center relative'>
             <Link className='text-[#F18DF2] font-bold text-3xl lg:text-5xl' href="/">Ai Lap</Link>
-            <ul onClick={(e)=>{if(e.target.tagName === "A"){setToggel(false)}}} className={`text-white flex flex-col lg:flex-row items-center gap-5 lg:w-auto absolute lg:static top-full start-0 bottom-0 overflow-hidden lg:h-auto bg-black lg:bg-transparent mt-2 lg:mt-0 h-[calc(100vh-80px)] py-2 transition-all duration-500 ${toggel ? "w-full" : "w-0"}`}>
+            <ul onClick={(e)=>{if(e.target.tagName === "A"){setToggel(false)}}} className={`text-white flex flex-col lg:flex-row items-center gap-5 lg:w-auto absolute lg:static top-full start-0 bottom-0 overflow-hidden lg:h-auto bg-black lg:bg-transparent mt-2 lg:mt-0 h-[calc(100dvh-61px)] py-2 transition-all duration-500 ${toggel ? "w-full" : "w-0"}`}>
                 {
                     NavLinks.map((link)=>(<li key={link.path} className={`relative pb-1 before:bg-[#F18DF2] before:bottom-0 before:start-0 before:w-0 hover:before:w-[85%] ${path === link.path ? "before:w-[85%]" : null} before:transition-all before:duration-300 before:h-[1px] before:absolute`}><Link href={link.path}>{link.content}</Link></li>))
                 }
